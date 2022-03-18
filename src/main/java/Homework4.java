a9bc578ebef3e496d3ac1779cbb0dc4fb536832e
ggonza55 <ggonza55@uic.edu>
2022-03-18 17:02:48 -0500

    Improved appearance and added more text files

--- src/main/java/Homework4.java
+++ src/main/java/Homework4.java
@@ -1,7 +1,9 @@
 import java.io.File;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
@@ -42,6 +44,8 @@ public class Homework4 extends Application {
 	private final BackgroundImage BG_IMAGE = new BackgroundImage(new Image("file:src/main/resources/images/background.png"), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
 	private final Background CARBON_BG = new Background(BG_IMAGE);
 	private final String SET_TEXT_WHITE = "-fx-text-fill: white";
+	private double standardWidth;
+	private double standardHeight;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -75,14 +79,20 @@ public class Homework4 extends Application {
 		Button guestButton = new Button("Guest Login");
 		Button agentButton = new Button("Agent Login");
 		HBox loginHBox = new HBox(guestButton, agentButton);
+		for (Node hButton : loginHBox.getChildren()) {
+			Button ourbutton = (Button)hButton;
+			ourbutton.setStyle("-fx-background-color: #4b92db; -fx-background-radius: 10px; -fx-padding: 10 10 10 10; -fx-text-fill: #ffffff; -fx-border-color: #ffffff; -fx-border-width: 2; -fx-border-radius: 10px");
+			ourbutton.setFont(Font.loadFont(BAUHAUS_FONT, 18));
+		}
 		loginHBox.setAlignment(Pos.CENTER); //Center the horizontal button box
 		loginHBox.setSpacing(15); //Give the buttons some space
 
 		//Vbox to arrange everything vertically
 		VBox welcomeVBox = new VBox(foundationLogo, scpVBox, kioskMessage, loginHBox);
-		welcomeVBox.setAlignment(Pos.TOP_CENTER); //Center the vertical box
+		welcomeVBox.setAlignment(Pos.CENTER); //Center the vertical box
 		welcomeVBox.setSpacing(30);//Give the elements some space between eachother
 		welcomeVBox.setBackground(CARBON_BG);
+		welcomeVBox.setPadding(new Insets(30,30,30,30));
 
 		//Deal with button presses
 		guestButton.setOnAction(e->{
@@ -95,16 +105,37 @@ public class Homework4 extends Application {
 			Stage confirmationStage = new Stage();
 			confirmationStage.initModality(Modality.APPLICATION_MODAL);
 			confirmationStage.initOwner(primaryStage);
-			Label confirmationLabel = new Label("Are you sure you're an agent? our password detector is broken so please tell us the truth.");
+			confirmationStage.getIcons().addAll(primaryStage.getIcons());
+			confirmationStage.setTitle("You wouldn't lie, right?");
+			confirmationStage.setResizable(false);
+			Label confirmationLabel = new Label("Are you sure you're an agent? Our password detector is broken so please tell us the truth.");
+			confirmationLabel.setWrapText(true);
 			Button confirmButton = new Button("YES");
 			confirmButton.setOnAction(event->{isOfficial = true;confirmationStage.close();});
 			Button denyButton = new Button("NO");
 			denyButton.setOnAction(event->{isOfficial = false;confirmationStage.close();});
 			HBox optionButtonsHBox = new HBox(confirmButton, denyButton);
 			VBox confirmationVBox = new VBox(confirmationLabel, optionButtonsHBox);
-			Scene confirmationScene = new Scene(confirmationVBox, 300, 200);
+			
+			//Make it pretty
+			confirmationVBox.setAlignment(Pos.CENTER);
+			optionButtonsHBox.setAlignment(Pos.CENTER);
+			confirmationVBox.setSpacing(10);
+			optionButtonsHBox.setSpacing(10);
+			confirmationLabel.setTextAlignment(TextAlignment.CENTER);
+			confirmationVBox.setBackground(CARBON_BG);
+			confirmationLabel.setStyle("-fx-text-fill: #ffffff; -fx-padding: 0 10 0 10");
+			confirmationLabel.setFont(Font.loadFont(BAUHAUS_FONT, 14));
+			for (Node n : optionButtonsHBox.getChildren()) {
+				Button b = (Button)n;
+				b.setStyle("-fx-background-color: #4b92db; -fx-background-radius: 10px; -fx-padding: 0 10 0 10; -fx-text-fill: #ffffff; -fx-border-color: #ffffff; -fx-border-width: 2; -fx-border-radius: 10px;");
+				b.setFont(Font.loadFont(BAUHAUS_FONT, 20));
+			}
+
+			Scene confirmationScene = new Scene(confirmationVBox, 280, 100);
 			confirmationStage.setScene(confirmationScene);
 			confirmationStage.showAndWait();
+
 
 			if (isOfficial) { //Make sure its an official!
 				if (agentScene == null)
@@ -114,10 +145,13 @@ public class Homework4 extends Application {
 		});
 
 		//Create our welcome scene
-		welcomeScene = new Scene(welcomeVBox, 700,600);
+		welcomeScene = new Scene(welcomeVBox);
 		//Set the welcome scene on stage and display it
 		primaryStage.setScene(welcomeScene);
 		primaryStage.show();
+		//Save Scene size to use elsewhere
+		standardWidth = welcomeScene.getWidth();
+		standardHeight = welcomeScene.getHeight();
 	}
 
 	private Scene createFetcherScene(Stage givenStage) {
@@ -133,10 +167,10 @@ public class Homework4 extends Application {
 			givenStage.setScene(welcomeScene);
 		});
 		
-		HBox fileButtonHBox = new HBox();
+		VBox fileButtonVBox = new VBox();
 		for (String file : directoryFiles) {
 			Button fileButton = new Button(file);
-			fileButtonHBox.getChildren().add(fileButton);
+			fileButtonVBox.getChildren().add(fileButton);
 			fileButton.setOnAction(e->{
 				String fileContents;
 				try {
@@ -157,8 +191,22 @@ public class Homework4 extends Application {
 			});
 		}
 		Label availableFilesLabel = new Label("Available Files:");
-		VBox fetcherVBox = new VBox(returnToLoginButton, availableFilesLabel, fileButtonHBox);
-		Scene fetcherScene = new Scene(fetcherVBox, 700, 600);
+		VBox fetcherVBox = new VBox(returnToLoginButton, availableFilesLabel, fileButtonVBox);
+		//Make it look good
+		fileButtonVBox.setAlignment(Pos.CENTER);
+		fileButtonVBox.setSpacing(5);
+		fetcherVBox.setAlignment(Pos.CENTER);
+		fetcherVBox.setBackground(CARBON_BG);
+		for (Node n : fileButtonVBox.getChildren()) {
+			Button fButton = (Button)n;
+			fButton.setPrefWidth(300);
+			fButton.setStyle("-fx-background-color: #303030; -fx-background-radius: 10px; -fx-padding: 10 10 10 10; -fx-text-fill: #ffffff; -fx-border-color: #ffffff; -fx-border-width: 2; -fx-border-radius: 10px;");
+			fButton.setFont(Font.loadFont(BAUHAUS_FONT, 18));
+		}
+		returnToLoginButton.setStyle("-fx-background-color: #4b92db; -fx-background-radius: 10px; -fx-padding: 10 10 10 10; -fx-text-fill: #ffffff; -fx-border-color: #ffffff; -fx-border-width: 2; -fx-border-radius: 10px;");
+		returnToLoginButton.setFont(Font.loadFont(BAUHAUS_FONT, 18));
+
+		Scene fetcherScene = new Scene(fetcherVBox, standardWidth, standardHeight);
 		return fetcherScene;
 	}
 
@@ -167,8 +215,23 @@ public class Homework4 extends Application {
 		returnButton.setOnAction(e->givenStage.setScene(chosenScene));
 		fileName = new Label("Filename.");
 		fileContents = new Label("File Contents.");
-		VBox fileDisplayVBox = new VBox(returnButton, fileName, fileContents);
-		Scene fileDisplayScene = new Scene(fileDisplayVBox, 700, 600);
+		//Put file contents in a vbox to avoid it touching edges of window
+		HBox fileContentsHBox = new HBox(fileContents);
+		VBox fileDisplayVBox = new VBox(returnButton, fileName, fileContentsHBox);
+		
+		//Make it look good
+		fileDisplayVBox.setAlignment(Pos.CENTER);
+		fileDisplayVBox.setBackground(CARBON_BG);
+		fileDisplayVBox.setSpacing(10);
+		fileContents.setStyle("-fx-background-color: #303030; -fx-text-fill: #ffffff; -fx-padding: 0 10 0 10; -fx-font-size: 20;");
+		fileName.setFont(Font.loadFont(BAUHAUS_FONT, 20));
+		fileName.setStyle("-fx-text-fill: #ffffff");
+		returnButton.setStyle("-fx-background-color: #4b92db; -fx-background-radius: 10px; -fx-padding: 10 10 10 10; -fx-text-fill: #ffffff; -fx-border-color: #ffffff; -fx-border-width: 2; -fx-border-radius: 10px;");
+		returnButton.setFont(Font.loadFont(BAUHAUS_FONT, 18));
+		fileContentsHBox.setPadding(new Insets(0, 10, 0, 10));
+		fileContentsHBox.setAlignment(Pos.CENTER);
+
+		Scene fileDisplayScene = new Scene(fileDisplayVBox, standardWidth, standardHeight);
 		return fileDisplayScene;
 	}
 
